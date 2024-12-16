e41de5dc7d202c90c6ecafb5f897982006f87d93
kaetosh <59330834+kaetosh@users.noreply.github.com>
2024-12-16 18:15:15 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 16 14:42:39 2024
+
+@author: a.karabedyan
+"""
+
+from FileProcessorFactory import FileProcessorFactory
+
+def main():
+    file_type = "account_turnover"
+    processor = FileProcessorFactory.create_processor(file_type)
+    processor.process_start()
+    processor.process()
+    processor.process_end()
+
+if __name__ == "__main__":
+    main()
