a3e6ab88203240441f7e5251a4656d1fa9463cb9
kaetosh <59330834+kaetosh@users.noreply.github.com>
2024-12-16 18:14:51 +0300

    Create Register1C.py

---
+++ Register1C.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 16 15:26:33 2024
+
+@author: a.karabedyan
+"""
+
+import collections
+
+Register1C = collections.namedtuple('Register1C', ['type_register'])
