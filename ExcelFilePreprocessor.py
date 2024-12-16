f09673bb239d6061b009a112cba10edf84e5c22f
kaetosh <59330834+kaetosh@users.noreply.github.com>
2024-12-16 18:16:16 +0300

    Create ExcelFilePreprocessor.py

---
+++ ExcelFilePreprocessor.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 16 16:35:32 2024
+
+@author: a.karabedyan
+"""
+
+import openpyxl
+
+class ExcelFilePreprocessor:
+    
+    def __init__(self, folder_path):
+        self.folder_path = folder_path
+    
+    def preprocessor_openpyxl(self):
+        files = list(self.folder_path.iterdir())
+        for oFile in files:
+            if oFile.suffix.lower() in ('.xls', '.xlsx'):
+                workbook = None
+                try:
+                    workbook = openpyxl.load_workbook(oFile)
+                except Exception as e:
+                    print(f'''{oFile}: Ошибка обработки файла. Возможно открыт обрабатываемый файл. Закройте этот файл и снова запустите скрипт.
+                                          Ошибка: {e}''')
+                continue  # Пропускаем файл и продолжаем с другими файлами
+
+                sheet = workbook.active
+            
+                # Снимаем объединение ячеек
+                merged_cells_ranges = list(sheet.merged_cells.ranges)
+                for merged_cell_range in merged_cells_ranges:
+                    sheet.unmerge_cells(str(merged_cell_range))
+            
+                # Столбец с уровнями группировок
+                sheet.insert_cols(idx=1)
+                for row_index in range(1, sheet.max_row + 1):
+                    cell = sheet.cell(row=row_index, column=1)
+                    cell.value = sheet.row_dimensions[row_index].outline_level
+                sheet['A1'] = "Уровень"
+                workbook.save(oFile)
+                #workbook.close()
