fef2fcaa94ca879e958db6547989b2f569d229f0
kaetosh <59330834+kaetosh@users.noreply.github.com>
2024-12-16 18:16:41 +0300

    Create ExcelFileConverter.py

---
+++ ExcelFileConverter.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 16 16:18:41 2024
+
+@author: a.karabedyan
+"""
+
+import win32com.client
+
+
+class ExcelFileConverter:
+
+    def __init__(self, excel_files):
+        self.excel_files = excel_files
+    
+    def save_as_xlsx_no_alert(self):
+        excel_app = win32com.client.Dispatch('Excel.Application')
+        excel_app.Visible = False
+        excel_app.DisplayAlerts = False
+        
+        for oFile in self.excel_files:
+            self.convert_file(excel_app, oFile)
+                
+        excel_app.Quit()
+        print('Файлы пересохранены')
+
+    def convert_file(self, excel_app, oFile):
+        wb = excel_app.Workbooks.Open(str(oFile))
+        new_file_path = oFile.stem + '.xlsx'
+        wb.SaveAs(str(new_file_path), FileFormat=51)
+        wb.Close(SaveChanges=False)
