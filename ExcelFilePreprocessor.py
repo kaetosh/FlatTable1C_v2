d23e0655dbbf6531ae1fb5eb6770143e2ce0f2d0
kaetosh <59330834+kaetosh@users.noreply.github.com>
2024-12-17 18:15:44 +0300

    Update ExcelFilePreprocessor.py

--- ExcelFilePreprocessor.py
+++ ExcelFilePreprocessor.py
@@ -9,33 +9,32 @@ import openpyxl
 
 class ExcelFilePreprocessor:
     
-    def __init__(self, folder_path):
-        self.folder_path = folder_path
+    def __init__(self, excel_files):
+        self.excel_files = excel_files
     
     def preprocessor_openpyxl(self):
-        files = list(self.folder_path.iterdir())
-        for oFile in files:
-            if oFile.suffix.lower() in ('.xls', '.xlsx'):
-                workbook = None
-                try:
-                    workbook = openpyxl.load_workbook(oFile)
-                except Exception as e:
-                    print(f'''{oFile}: Ошибка обработки файла. Возможно открыт обрабатываемый файл. Закройте этот файл и снова запустите скрипт.
-                                          Ошибка: {e}''')
-                continue  # Пропускаем файл и продолжаем с другими файлами
 
-                sheet = workbook.active
-            
-                # Снимаем объединение ячеек
-                merged_cells_ranges = list(sheet.merged_cells.ranges)
-                for merged_cell_range in merged_cells_ranges:
-                    sheet.unmerge_cells(str(merged_cell_range))
-            
-                # Столбец с уровнями группировок
-                sheet.insert_cols(idx=1)
-                for row_index in range(1, sheet.max_row + 1):
-                    cell = sheet.cell(row=row_index, column=1)
-                    cell.value = sheet.row_dimensions[row_index].outline_level
-                sheet['A1'] = "Уровень"
-                workbook.save(oFile)
-                #workbook.close()
+        for oFile in self.excel_files:
+            workbook = None
+            try:
+                workbook = openpyxl.load_workbook(oFile)
+            except Exception as e:
+                print(f'''{oFile}: Ошибка обработки файла. Возможно открыт обрабатываемый файл. Закройте этот файл и снова запустите скрипт.
+                                      Ошибка: {e}''')
+            continue  # Пропускаем файл и продолжаем с другими файлами
+
+            sheet = workbook.active
+        
+            # Снимаем объединение ячеек
+            merged_cells_ranges = list(sheet.merged_cells.ranges)
+            for merged_cell_range in merged_cells_ranges:
+                sheet.unmerge_cells(str(merged_cell_range))
+        
+            # Столбец с уровнями группировок
+            sheet.insert_cols(idx=1)
+            for row_index in range(1, sheet.max_row + 1):
+                cell = sheet.cell(row=row_index, column=1)
+                cell.value = sheet.row_dimensions[row_index].outline_level
+            sheet['A1'] = "Уровень"
+            workbook.save(oFile)
+            #workbook.close()
