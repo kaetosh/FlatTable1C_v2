ea345e3724a9d4cce3eaf61bb5ac90723588bf58
kaetosh <59330834+kaetosh@users.noreply.github.com>
2024-12-17 18:16:34 +0300

    Create ErrorClasses.py

---
+++ ErrorClasses.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec 17 16:32:09 2024
+
+@author: a.karabedyan
+"""
+
+class NoExcelFilesError(Exception):
+    """Исключение, возникающее, когда нет доступных Excel файлов для обработки."""
+    pass
