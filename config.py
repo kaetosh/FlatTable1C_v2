44c2a5026884f082127649fb07078735265150d0
kaetosh <59330834+kaetosh@users.noreply.github.com>
2024-12-24 18:12:38 +0300

    Update config.py

--- config.py
+++ config.py
@@ -25,6 +25,7 @@ new_names = ['Дебет_начало',
                  'Дебет_конец',
                  'Кредит_конец']
 
+# поля ОСВ в качестве первичного залоговка таблицы
 filds_osv_upp = FieldsRegister(analytics='Субконто',
                                type_connection = 'Вид связи КА за период', 
                                start_debit_balance = 'Нач. сальдо деб.',
@@ -34,4 +35,47 @@ filds_osv_upp = FieldsRegister(analytics='Субконто',
                                end_debit_balance = 'Кон. сальдо деб.',
                                end_credit_balance = 'Кон. сальдо кред.')
 
-osv_upp = Register_1C(FieldsRegister_upp = filds_osv_upp)
+filds_osv_notupp = FieldsRegister(analytics='Счет',
+                                  quantity='Показа-\nтели',
+                                  start_debit_balance = 'Сальдо на начало периода',
+                                  debit_turnover = 'Обороты за период',
+                                  end_debit_balance = 'Сальдо на конец периода	')
+
+osv_filds = Register_1C(filds_osv_upp, filds_osv_notupp)
+
+
+# поля Обороты счета в качестве первичного залоговка таблицы
+filds_turnover_upp = FieldsRegister(analytics='Субконто',
+                                    type_connection = 'Вид связи КА за период', 
+                                    start_debit_balance = 'Нач. сальдо деб.',
+                                    start_credit_balance = 'Нач. сальдо кред.', 
+                                    debit_turnover = 'Деб. оборот',
+                                    credit_turnover = 'Кред. оборот', 
+                                    end_debit_balance = 'Кон. сальдо деб.',
+                                    end_credit_balance = 'Кон. сальдо кред.')
+
+filds_turnover_notupp = FieldsRegister(analytics='Счет',
+                                  quantity='Показа-\nтели',
+                                  start_debit_balance = 'Начальное сальдо Дт',
+                                  start_credit_balance = 'Начальное сальдо Кт', 
+                                  debit_turnover = 'Оборот Дт',
+                                  credit_turnover = 'Оборот Кт', 
+                                  end_debit_balance = 'Конечное сальдо Дт',
+                                  end_credit_balance = 'Конечное сальдо Кт')
+
+turnover_filds = Register_1C(filds_turnover_upp, filds_turnover_notupp)
+
+# поля Анализ счета в качестве первичного залоговка таблицы
+filds_analisys_upp = FieldsRegister(analytics='Счет',
+                                    type_connection = 'Вид связи КА за период',
+                                    corresponding_account = 'Кор.счет',
+                                    debit_turnover = 'С кред. счетов',
+                                    credit_turnover = 'В дебет счетов')
+
+filds_analisys_notupp = FieldsRegister(analytics='Счет',
+                                       corresponding_account = 'Кор. Счет',
+                                       quantity='Показа-\nтели',
+                                       debit_turnover = 'Дебет',
+                                       credit_turnover = 'Кредит')
+
+analisys_filds = Register_1C(filds_analisys_upp, filds_analisys_notupp)
