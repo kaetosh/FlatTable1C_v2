89bd8c0bb2ced7cccf4873528b7296913a82c985
kaetosh <59330834+kaetosh@users.noreply.github.com>
2024-12-25 18:07:01 +0300

    Update ExcelFilePreprocessor.py

--- ExcelFilePreprocessor.py
+++ ExcelFilePreprocessor.py
@@ -6,11 +6,13 @@ Created on Mon Dec 16 16:35:32 2024
 """
 
 import openpyxl
+from config import analisys_filds
 
 class ExcelFilePreprocessor:
     
-    def __init__(self, excel_files):
+    def __init__(self, excel_files, file_type):
         self.excel_files = excel_files
+        self.file_type = file_type
     
     def preprocessor_openpyxl(self):
 
@@ -21,7 +23,6 @@ class ExcelFilePreprocessor:
             except Exception as e:
                 print(f'''{oFile}: Ошибка обработки файла. Возможно открыт обрабатываемый файл. Закройте этот файл и снова запустите скрипт.
                                       Ошибка: {e}''')
-            continue  # Пропускаем файл и продолжаем с другими файлами
 
             sheet = workbook.active
         
@@ -35,6 +36,21 @@ class ExcelFilePreprocessor:
             for row_index in range(1, sheet.max_row + 1):
                 cell = sheet.cell(row=row_index, column=1)
                 cell.value = sheet.row_dimensions[row_index].outline_level
-            sheet['A1'] = "Уровень"
+            
+            if self.file_type == 'account_analisys':
+                sheet.insert_cols(idx=2)
+                for row in sheet.iter_rows(values_only=True):
+
+                    found_value = next((value for value in [analisys_filds.upp.corresponding_account,
+                                                            analisys_filds.notupp.corresponding_account] if value in row), None)
+                    
+                    if found_value is not None:
+                        kor_schet_col_index = row.index(found_value) + 1  # We add 1 because indexing starts from 0
+                        # Мы заполняем новый столбец значениями, основанными на форматировании ячеек курсивом
+                        for row_index in range(2, sheet.max_row + 1):  # We start with 2 to skip the title
+                            kor_schet_cell = sheet.cell(row=row_index, column=kor_schet_col_index)
+                            new_cell = sheet.cell(row=row_index, column=2)
+                            new_cell.value = 1 if kor_schet_cell.font and kor_schet_cell.font.italic else 0
+                        break
+
             workbook.save(oFile)
-            #workbook.close()
