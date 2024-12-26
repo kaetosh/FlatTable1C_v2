1c8e4b275d394c3001acc045bc2227ac605df378
kaetosh <59330834+kaetosh@users.noreply.github.com>
2024-12-26 18:11:49 +0300

    Update FileProcessor.py

--- FileProcessor.py
+++ FileProcessor.py
@@ -9,8 +9,8 @@ import os
 from typing import List, Dict
 import pandas as pd
 from pathlib import Path
-from Register1C import Register_1C
-from config import name_account_balance_movements, sign_1c_upp, sign_1c_not_upp, new_names, osv_filds, turnover_filds, analisys_filds
+from Register1C import Register_1C, Table_storage
+from config import new_names, osv_filds, turnover_filds, analisys_filds
 from ErrorClasses import NoExcelFilesError
 from ExcelFileConverter import ExcelFileConverter
 from ExcelFilePreprocessor import ExcelFilePreprocessor
@@ -71,11 +71,12 @@ class IFileProcessor:
                     break
     
             if match_index is not None:
-                self.sign_1c = register.get_attribute_name_by_value(first_valid_value)
-    
+
                 # Устанавливаем заголовки и очищаем данные
-                df.columns = df.iloc[match_index].astype(str)
-                df = df.loc[:, df.columns.notna()]
+                df.columns = df.iloc[match_index]
+                # df = df.loc[:, df.columns.notna()]
+                # df = df.loc[:, df.columns.notna() | df.apply(lambda x: x.astype(str).str.contains(r'\d').any())]
+                # df.columns = df.columns.astype(str)
                 df = df.drop(df.index[0:(match_index + 1)])
                 df.dropna(axis=0, how='all', inplace=True)
                 df.dropna(axis=1, how='all', inplace=True)
@@ -87,9 +88,11 @@ class IFileProcessor:
                 df['Исх.файл'] = oFile.name
                 
                 # запишем таблицу в словарь
-                self.dict_df[oFile.name] = df
+                sign_1c = register.get_attribute_name_by_value(first_valid_value)
+                self.dict_df[oFile.name] = Table_storage(table=df, register=register, sign_1C=sign_1c)
             else:
                 self.empty_files.append(oFile.name)
+            
     
     def special_table_header(self) -> None:
         pass
@@ -98,54 +101,53 @@ class IFileProcessor:
         print('Закончили обработку')
 
 class AccountTurnoverProcessor(IFileProcessor):
-    
-    def find_turnover_index(self, turnover_type: str, df: pd.DataFrame) -> int or False:
-        try:
-            # Ищем первый существующий индекс
-            turnover_index: int = next(df.columns.get_loc(i) for i in name_account_balance_movements[turnover_type] if i in df.columns)
-            
-            # Проверяем, является ли файл выгрузкой из 1С УПП
-            if df.columns[turnover_index] == name_account_balance_movements[turnover_type][1]:
-                self.sign_1c = sign_1c_upp
-            return turnover_index
-        except StopIteration:
-            return False  # Если ничего не найдено
-    
+        
     def special_table_header(self) -> None:
-
-        for i in self.dict_df:
-            df = self.dict_df[i]
-            
+        for file in self.dict_df:
+            df = self.dict_df[file].table
+            sign_1c = self.dict_df[file].sign_1C
             indices_to_rename: List[int] = []
+            filds_account_turnover = getattr(turnover_filds, sign_1c)
             
-            for turnover_type in name_account_balance_movements.keys():
-                index_turnover_type: int or False = self.find_turnover_index(turnover_type, df)
-                setattr(self, f"{turnover_type}_index",index_turnover_type)
-                indices_to_rename.append(index_turnover_type)
+            df = df.loc[:, df.columns.notna()]
+            df.columns = df.columns.astype(str)
             
-            if any(col in df.columns for col in name_account_balance_movements['debit_turnover']):
+            for turnover_type in filds_account_turnover:
+                try:
+                    name_atribute = turnover_filds.get_inner_attribute_by_value(turnover_type)
+                    index_turnover_type: int or False = df.columns.get_loc(turnover_type) if turnover_type in df.columns else False
+                    self.dict_df[file].set_index_column(name_atribute, index_turnover_type)
+                    if ('debit' in name_atribute) or ('credit' in name_atribute) and index_turnover_type:
+                        indices_to_rename.append(index_turnover_type)
+                except TypeError:
+                    continue # Пропускаем, если turnover_type == None
+                except StopIteration:
+                    continue  # Если ничего не найдено
+
+            if filds_account_turnover.debit_turnover in df.columns:
                 # Определяем верхнюю границу для добавления префикса 'до'
-                upper_bound_index: int = self.credit_turnover_index or self.end_debit_balance_index or self.end_credit_balance_index
+                debit_turnover_index: int = getattr(self.dict_df[file], 'index_column_debit_turnover', None)
+                credit_turnover_index: int = getattr(self.dict_df[file], 'index_column_credit_turnover', None)
+                end_debit_balance_index: int = getattr(self.dict_df[file], 'index_column_end_debit_balance', None)
+                end_credit_balance_index: int = getattr(self.dict_df[file], 'index_column_end_credit_balance', None)
+                upper_bound_index: int = credit_turnover_index or end_debit_balance_index or end_credit_balance_index
             
                 # Создаем новый список названий столбцов с префиксом 'до'
                 list_do_columns: List[str] = []
                 for idx, col in enumerate(df.columns):
                     # Если нашли индекс 'дебетового оборота', добавляем префикс 'до' при выполнении условий
-                    if self.debit_turnover_index is not None and idx > self.debit_turnover_index and (upper_bound_index is None or idx < upper_bound_index):
+                    if debit_turnover_index is not None and idx > debit_turnover_index and (upper_bound_index is None or idx < upper_bound_index):
                         list_do_columns.append(f'{col}_до')
                     else:
                         list_do_columns.append(col)
                 # Обновляем названия столбцов в DataFrame
                 df.columns = list_do_columns
             
-            if any(col in df.columns for col in name_account_balance_movements['credit_turnover']):
+            if filds_account_turnover.credit_turnover in df.columns:
                 list_ko_columns: List[str] = []
                 
-                # Находим индекс 'КредитОборот'
-                credit_turnover_index: int = df.columns.get_loc('КредитОборот') if 'КредитОборот' in df.columns else None
-                
                 # Определяем границы для добавления префикса 'ко'
-                end_balances_index: int = max(self.end_debit_balance_index or -1, self.end_credit_balance_index or -1)  # Определяем конец диапазона
+                end_balances_index: int = max(end_debit_balance_index or -1, end_credit_balance_index or -1)  # Определяем конец диапазона
                 
                 for idx, col in enumerate(df.columns):
                     # Добавляем префикс, если индекс в нужном диапазоне
@@ -156,45 +158,90 @@ class AccountTurnoverProcessor(IFileProcessor):
                 # Обновляем названия столбцов в DataFrame
                 df.columns = list_ko_columns
             
-            # переименуем первые два столбца
-            df.columns.values[0] = 'Уровень'
-            
             # Получаем текущие имена столбцов
             current_columns: List[str] = df.columns.tolist()
             
             # Создаем словарь с новыми именами для желаемых индексов
             rename_dict: Dict[str, str] = {current_columns[i]: new_names[j] for j, i in enumerate(indices_to_rename) if i}
+            
+            print('rename_dict', rename_dict)
         
             # Переименовываем столбцы
             df.rename(columns=rename_dict, inplace=True)
             
-            # название файла
-            df['Исх.файл'] = oFile.name
-            
             # запишем таблицу в словарь
-            self.dict_df[oFile.name] = df
+            self.dict_df[file].table = df
             
         # список проблемных файлов и проч удалить потом
         # for i in self.dict_df:
-        #     self.dict_df[i].to_excel(f'{i}_обраб.xlsx')
+        #     self.dict_df[i].table.to_excel(f'{i}_обраб.xlsx')
         # print('empty_files', self.empty_files)
 
 class AccountOSVProcessor(IFileProcessor):
-    def table_header_OSV(self) -> None:
-        if not self.excel_files:
-            raise NoExcelFilesError('Нет доступных Excel файлов для обработки.')
-        for oFile in self.excel_files:
-            df: pd.DataFrame = pd.read_excel(oFile)
+    def special_table_header(self) -> None:
+        for file in self.dict_df:
+            df = self.dict_df[file].table
+            sign_1c = self.dict_df[file].sign_1C
+            indices_to_rename: List[int] = []
+            filds_osv = getattr(osv_filds, sign_1c)
             
-            # Получаем индекс строки, содержащей target_value (значение)
-            index_for_columns: int or None = None
-            target_values: set = {value for sublist in name_account_balance_movements.values() for value in sublist} # Извлекаем все значения
-            indices: pd.core.indexes.base.Index = df.index[df.apply(lambda row: row.isin(target_values).any(), axis=1)]
-            if not indices.empty:
-                index_for_columns = indices[0]  # Получаем первый индекс
-            else:
-                self.empty_files.append(oFile.name)
-                continue
+            # for i in df.columns:
+            #     match i:
+            #         case (filds_osv.start_debit_balance | filds_osv.debit_turnover | filds_osv.end_debit_balance):
+            #             if df.iloc[0, i] == 'Дебет':
+            #                 df.rename(columns={i: f'{i}_Дебет'}, inplace=True)
+            #             elif df.iloc[0, i] == 'Кредит':
+            #                 df.rename(columns={i: f'{i}_Кредит'}, inplace=True)
+            #             else:
+            #                 raise ValueError(f"Неизвестный тип файла: {file}")
+            #         case 'nan':
+            #             pass
+        
+        
+        counters = {'Дебет': 0, 'Кредит': 0}
+        def update_account_list(item):
+            if item in counters:
+                # Увеличиваем счетчик для 'Дебет' или 'Кредит'
+                counters[item] += 1
+                # Возвращаем обновленное значение элемента
+                return f"{item}_{['начало', 'оборот', 'конец'][counters[item] - 1]}"
+            return item
+        
+        updated_list = [update_account_list(item) for item in df.iloc[0]]
+        
+        name_col = df.columns.to_list()
+        # Список значений для замены
+        replacement_values = ['Дебет_начало', 'Кредит_начало', 'Дебет_оборот', 'Кредит_оборот', 'Дебет_конец', 'Кредит_конец']
+        
+        # Замена значений в list_1
+        for index, value in enumerate(updated_list):
+            if value in replacement_values:
+                name_col[index] = value
+        
+        df.columns = name_col
+        
+        df = df.loc[:, df.columns.notna()]
+        print(df.columns)
+        print()
+        print(df.head(3))
+        df.columns = df.columns.astype(str)
+        df.to_excel('1.xlsx')
+        
+        # список проблемных файлов и проч удалить потом
+        # for i in self.dict_df:
+        #     self.dict_df[i].table.to_excel(f'{i}_обраб.xlsx')
+        
+        # #sign_1c = sign_1c_upp
+        # list_columns_necessary = ['Уровень', sign_1c, 'Вид связи КА за период', 'Дебет_начало', 'Кредит_начало', 'Дебет_оборот', 'Кредит_оборот', 'Дебет_конец', 'Кредит_конец'] # список необходимых столбцов
+        # list_columns_necessary_error = ['Уровень', sign_1c, 'Дебет_начало', 'Кредит_начало', 'Дебет_оборот', 'Кредит_оборот', 'Дебет_конец', 'Кредит_конец'] # список необходимых столбцов
+        # try:
+        #     df = df[list_columns_necessary].copy()
+        # except KeyError as e:
+        #     if 'Субконто' in e.args[0]:
+        #         sign_1c = sign_1c_not_upp
+        #         list_columns_necessary_error = ['Уровень', sign_1c, 'Дебет_начало', 'Кредит_начало', 'Дебет_оборот', 'Кредит_оборот', 'Дебет_конец', 'Кредит_конец'] # список необходимых столбцов
+        #     df = df[list_columns_necessary_error].copy()
+        #     logger_with_spinner(f'{file_excel}: ОТСУТСТВУЕТ СТОЛБЕЦ Вид связи КА за период')
             
 class AccountAnalisysProcessor(IFileProcessor):
     pass
